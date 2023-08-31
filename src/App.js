5b471adb122a46be7c6ac3b99316ade54315d0c2
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-30 21:08:35 -0300

    finalizando o conceito de apis

--- src/App.js
+++ src/App.js
@@ -1,33 +1,87 @@
 import './App.css';
 
 import { useState, useEffect } from 'react'
+import { useFetch } from './hooks/useFetch';
+import useDelete from './hooks/useDelete';
 
 const url = 'http://localhost:5000/products'
 
 function App() {
   const [ products, setProducts ] = useState([])
+  const [ name, setName] = useState('')
+  const [ price, setPrice ] = useState('')
+
+
+  const { deleteProduct, loading: deleteLoading, error } = useDelete();
+  const { data, loading } = useFetch(url)
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url)
-      const data = await res.json()
-      setProducts(data)
+    setProducts(data)
+
+  }, [data])
+
+
+  const handleDelete = async (id) => {
+    await deleteProduct(url, id);
+    // Optionally, you can update products state here if needed
+    setProducts((prevProducts) => prevProducts.filter(product => product.id !== id));
+  };
+
+
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
     }
 
-    fetchData()
-  }, [])
-  
-  console.log(products);
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    });
+
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    setName("")
+    setPrice("")
+
+  }
+
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      <ul>
-        {products.map((product) => (
+        {loading && <p>Carregando dados...</p>}
+        {!loading && (
+          <ul>
+        {products && products.map((product) => (
           <li key={product.id}>
             {product.name} - R$: {product.price}
+            <button onClick={() => handleDelete(product.id)}>Remover</button>
           </li>
         ))}
       </ul>
+        )}
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            NOME:
+            <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            PREÇO:
+            <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value='Criar' />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+// custom hook
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null)
+
+    const [ loading, setLoading ] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+
+      const res = await fetch(url);
+      const json = await res.json();
+
+      setData(json);
+      setLoading(false)
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading };
+};
